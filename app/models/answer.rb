1b367f5c2d311c7a22eae11d0574194f663fe9ef
ATitkov <art-eastwood@yandex.ru>
2016-11-29 20:25:11 +0300

    association announced first

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ApplicationRecord
- validates :body, length: { minimum: 2 }, presence: true
  belongs_to :question, foreign_key: "question_id"
+ validates :body, length: { minimum: 2 }, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-  validates :title, :body, presence: true
   has_many :answers, foreign_key: "answer_id", dependent: :destroy	
+  validates :title, :body, presence: true
 end
